5e35691cd81a9d92ebc79e712524cfa8ad871dcc
Shelly Goebel <shellygoebel972@gmail.com>
2019-07-31 19:40:32 -0500

    fixed repo issue

---
+++ src/main/java/com/shellys_capstone/shellyscapstone/ShellysCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.shellys_capstone.shellyscapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellysCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellysCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shellys_capstone/shellyscapstone/ShellysCapstoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shellys_capstone.shellyscapstone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShellysCapstoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
