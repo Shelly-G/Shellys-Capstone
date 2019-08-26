741d869bc2194ec35c3dbc097d4e7d7b6ebf1beb
Shelly Goebel <shellygoebel972@gmail.com>
2019-08-25 19:28:20 -0500

    User page first commit - not all working yet

---
+++ src/main/java/com/shellys_capstone/shellyscapstone/controllers/UserController.java
@@ -0,0 +1,43 @@
+package com.shellys_capstone.shellyscapstone.controllers;
+
+
+import com.shellys_capstone.shellyscapstone.models.forms.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.validation.Valid;
+
+
+
+@SuppressWarnings("ALL")
+@Controller
+@RequestMapping("user")
+
+public class UserController {
+    @SuppressWarnings("deprecation")
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model){
+        model.addAttribute(new User());
+        model.addAttribute("title", "Register");
+        return "user/add";
+}
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid User user, Errors errors) {
+
+        model.addAttribute(user);
+
+        if (user.getPassword() !=null && user.getPassword().equals(verify)){
+            model.addAttribute("name", user.getUsername());
+            return "user/index";
+        }
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("error", "Passwords do not match");
+        return "user/add";
+    }
+
+}
+
